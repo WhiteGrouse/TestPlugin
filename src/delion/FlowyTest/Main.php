44afbdd84f9cb4f6d3fc2302eee420cbfd9bc11d
WhiteGrouse <39170892+WhiteGrouse@users.noreply.github.com>
2020-06-06 03:44:54 +0900

    source upload

---
+++ src/delion/FlowyTest/Main.php
@@ -0,0 +1,42 @@
+<?php
+namespace delion\FlowyTest;
+
+use pocketmine\plugin\PluginBase;
+use flowy\Flowy;
+use flowy\standard\Standard;
+use function flowy\{listen, start};
+use function flowy\standard\delay;
+
+class Main extends PluginBase {
+  function onEnable() {
+    Flowy::bootstrap();
+    Standard::bootstrap();
+
+    $stream = start($this);
+    $stream->run(function($stream) {
+      $this->getLogger()->info("sleeping...");
+      yield from delay($this->getScheduler(), 20 * 10);
+      $this->getLogger()->info("Hello!!");
+
+      while(true) {
+        $event = yield listen(PlayerJoinEvent::class);
+        $player = $event->getPlayer();
+
+        $playerStream = $stream->filter(function($ev) use ($player) {
+          return !($ev instanceof PlayerEvent) || $ev->getPlayer() === $player;
+        })->stream();
+
+        $playerStream->run(function($stream) {
+          $event = yield listen(PlayerQuitEvent::class);
+          $stream->close();
+        });
+        $playerStream->run(function($stream) use ($player) {
+          $event = yield listen(PlayerJumpEvent::class);
+          yield from delay($this->getScheduler(), 20 * 3);
+          $player->sendMessage("rejump!!");
+          $player->jump();
+        });
+      }
+    });
+  }
+}
